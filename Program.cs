8f2803465a81fc05bdd10412563adc72c293e31f
zaraa <DittGithubEpost>
2025-02-05 14:36:37 +0100

    Ändringar i projektet då det blev mycket errors igen..

--- Program.cs
+++ Program.cs
@@ -5,9 +5,15 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+// Set the base path for the application
+app.UsePathBase("/hello");
+
+// Enable routing
 app.UseRouting();
 
 // Map the controllers
 app.MapControllers();
+
+app.MapGet("/", () => "Welcome to EnkrypteringAPI!");
 
 app.Run();
