a867b57e208d3f8857b54a7513cbb55f3c9679ac
zaraa <DittGithubEpost>
2025-02-05 14:03:52 +0100

    Add project files.

---
+++ Controller/Controller.cs
@@ -0,0 +1,13 @@
+﻿namespace krypteringAPI.Controller;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("")]
+[ApiController]
+public class HelloController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello from the API!");
+    }
+}
